e30a8d29ef21097dff8b706f15772f5caf5a3d63
haoweiwang0 <15011424380@163.com>
2024-04-02 15:06:39 +0800

    modified:   src/test.py

--- src/test.py
+++ src/test.py
@@ -17,6 +17,7 @@ from easydict import EasyDict as edict
 import cv2
 import torch
 import numpy as np
+from tqdm.auto import tqdm
 
 sys.path.append('../')
 
@@ -109,62 +110,64 @@ if __name__ == '__main__':
 
     test_dataloader = create_test_dataloader(configs)
     with torch.no_grad():
-        for batch_idx, (img_paths, imgs_bev) in enumerate(test_dataloader):
-            input_imgs = imgs_bev.to(device=configs.device).float()
-            t1 = time_synchronized()
-            outputs = model(input_imgs)
-            t2 = time_synchronized()
-            detections = post_processing_v2(outputs, conf_thresh=configs.conf_thresh, nms_thresh=configs.nms_thresh)
+        with tqdm(total=len(test_dataloader)) as tbar:
+            for batch_idx, (img_paths, imgs_bev) in enumerate(test_dataloader):
+                input_imgs = imgs_bev.to(device=configs.device).float()
+                t1 = time_synchronized()
+                outputs = model(input_imgs)
+                t2 = time_synchronized()
+                detections = post_processing_v2(outputs, conf_thresh=configs.conf_thresh, nms_thresh=configs.nms_thresh)
 
-            img_detections = []  # Stores detections for each image index
-            img_detections.extend(detections)
+                img_detections = []  # Stores detections for each image index
+                img_detections.extend(detections)
 
-            img_bev = imgs_bev.squeeze() * 255
-            img_bev = img_bev.permute(1, 2, 0).numpy().astype(np.uint8)
-            img_bev = cv2.resize(img_bev, (configs.img_size, configs.img_size))
-            for detections in img_detections:
-                if detections is None:
-                    continue
-                # Rescale boxes to original image
-                detections = rescale_boxes(detections, configs.img_size, img_bev.shape[:2])
-                for x, y, w, l, im, re, *_, cls_pred in detections:
-                    yaw = np.arctan2(im, re)
-                    # Draw rotated box
-                    kitti_bev_utils.drawRotatedBox(img_bev, x, y, w, l, yaw, cnf.colors[int(cls_pred)])
+                img_bev = imgs_bev.squeeze() * 255
+                img_bev = img_bev.permute(1, 2, 0).numpy().astype(np.uint8)
+                img_bev = cv2.resize(img_bev, (configs.img_size, configs.img_size))
+                for detections in img_detections:
+                    if detections is None:
+                        continue
+                    # Rescale boxes to original image
+                    detections = rescale_boxes(detections, configs.img_size, img_bev.shape[:2])
+                    for x, y, w, l, im, re, *_, cls_pred in detections:
+                        yaw = np.arctan2(im, re)
+                        # Draw rotated box
+                        kitti_bev_utils.drawRotatedBox(img_bev, x, y, w, l, yaw, cnf.colors[int(cls_pred)])
 
-            img_rgb = cv2.imread(img_paths[0])
-            calib = kitti_data_utils.Calibration(img_paths[0].replace(".png", ".txt").replace("image_2", "calib"))
-            objects_pred = predictions_to_kitti_format(img_detections, calib, img_rgb.shape, configs.img_size)
-            img_rgb = show_image_with_boxes(img_rgb, objects_pred, calib, False)
+                img_rgb = cv2.imread(img_paths[0])
+                calib = kitti_data_utils.Calibration(img_paths[0].replace(".png", ".txt").replace("image_2", "calib"))
+                objects_pred = predictions_to_kitti_format(img_detections, calib, img_rgb.shape, configs.img_size)
+                img_rgb = show_image_with_boxes(img_rgb, objects_pred, calib, False)
 
-            img_bev = cv2.flip(cv2.flip(img_bev, 0), 1)
+                img_bev = cv2.flip(cv2.flip(img_bev, 0), 1)
 
-            out_img = merge_rgb_to_bev(img_rgb, img_bev, output_width=608)
+                out_img = merge_rgb_to_bev(img_rgb, img_bev, output_width=608)
 
-            print('\tDone testing the {}th sample, time: {:.1f}ms, speed {:.2f}FPS'.format(batch_idx, (t2 - t1) * 1000,
-                                                                                           1 / (t2 - t1)))
+                tbar.set_description('time: {:.1f}ms, speed {:.2f}FPS'.format(batch_idx, (t2 - t1) * 1000,
+                                                                                            1 / (t2 - t1)))
+                tbar.update(1)
 
-            if configs.save_test_output:
-                if configs.output_format == 'image':
-                    img_fn = os.path.basename(img_paths[0])[:-4]
-                    cv2.imwrite(os.path.join(configs.results_dir, '{}.jpg'.format(img_fn)), out_img)
-                elif configs.output_format == 'video':
-                    if out_cap is None:
-                        out_cap_h, out_cap_w = out_img.shape[:2]
-                        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-                        out_cap = cv2.VideoWriter(
-                            os.path.join(configs.results_dir, '{}.avi'.format(configs.output_video_fn)),
-                            fourcc, 30, (out_cap_w, out_cap_h))
+                if configs.save_test_output:
+                    if configs.output_format == 'image':
+                        img_fn = os.path.basename(img_paths[0])[:-4]
+                        cv2.imwrite(os.path.join(configs.results_dir, '{}.jpg'.format(img_fn)), out_img)
+                    elif configs.output_format == 'video':
+                        if out_cap is None:
+                            out_cap_h, out_cap_w = out_img.shape[:2]
+                            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+                            out_cap = cv2.VideoWriter(
+                                os.path.join(configs.results_dir, '{}.avi'.format(configs.output_video_fn)),
+                                fourcc, 30, (out_cap_w, out_cap_h))
 
-                    out_cap.write(out_img)
-                else:
-                    raise TypeError
+                        out_cap.write(out_img)
+                    else:
+                        raise TypeError
 
-            if configs.show_image:
-                cv2.imshow('test-img', out_img)
-                print('\n[INFO] Press n to see the next sample >>> Press Esc to quit...\n')
-                if cv2.waitKey(0) & 0xFF == 27:
-                    break
+                if configs.show_image:
+                    cv2.imshow('test-img', out_img)
+                    print('\n[INFO] Press n to see the next sample >>> Press Esc to quit...\n')
+                    if cv2.waitKey(0) & 0xFF == 27:
+                        break
     if out_cap:
         out_cap.release()
     cv2.destroyAllWindows()
